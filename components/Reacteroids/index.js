769fabeca9438c6d8f27cbe132c7417f5a1b0af5
Jeffrey Aven <javen@avensolutions.com>
2023-08-07 08:29:46 +1000

    cleanup

--- components/Reacteroids/index.js
+++ components/Reacteroids/index.js
@@ -1,8 +1,2 @@
-// import React from 'react';
-// import { render } from 'react-dom';
 import { Reacteroids } from './Reacteroids';
-// import style from './style.css';
-
-// render(<Reacteroids />, document.getElementById('root'));
-
 export { Reacteroids };
